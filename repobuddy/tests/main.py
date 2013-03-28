bc3e793f92d448f78ee894e2de7529d1f10018e8
Ash <tuxdude@WingSaber>
2013-03-27 18:11:42 -0700

    Set GIT env variables for name and email for tests.
    Set the following 4 environment variables so that the tests run on a CI
    system (like travis) does not complain that username and email are not
    set:
    
    GIT_AUTHOR_NAME
    GIT_AUTHOR_EMAIL
    GIT_COMMITTER_NAME
    GIT_COMMITTER_EMAIL

--- repobuddy/tests/main.py
+++ repobuddy/tests/main.py
@@ -27,6 +27,11 @@ from repobuddy.tests.manifest_parser import ManifestParserTestSuite
 
 
 def run_tests():
+    _os.environ['GIT_AUTHOR_NAME'] = 'repobuddy-testbot'
+    _os.environ['GIT_AUTHOR_EMAIL'] = 'testbot@repobuddy'
+    _os.environ['GIT_COMMITTER_NAME'] = _os.environ['GIT_AUTHOR_NAME']
+    _os.environ['GIT_COMMITTER_EMAIL'] = _os.environ['GIT_COMMITTER_NAME']
+
     test_dir = _os.path.join(_os.getcwd(), 'testing-ground')
     tests = TestSuiteManager(test_dir)
 
