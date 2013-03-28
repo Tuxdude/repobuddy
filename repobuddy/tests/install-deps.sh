3c1d8306501db285e0bd2ddbbf15697c548dd4c4
Ash <tuxdude@StarScream>
2013-03-28 01:38:41 -0700

    Fixed buggy install-deps.sh

--- repobuddy/tests/install-deps.sh
+++ repobuddy/tests/install-deps.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 if python -c 'import sys as _sys; _sys.exit(_sys.version_info[0] != 2)'; then
     # Python 2
-    if python -c 'import sys as _sys; sys.exit(_sys.version_info[0] != 6)'; then
+    if python -c 'import sys as _sys; _sys.exit(_sys.version_info[1] != 6)'; then
         # Python 2.6
         pip install ordereddict unittest2
     fi
