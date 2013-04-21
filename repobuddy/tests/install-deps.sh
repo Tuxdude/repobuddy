616dddefd5558a879b3c98f1f2baf36f32316b04
Ash <tuxdude@Suse1520>
2013-04-21 02:29:22 -0700

    Update install-deps.sh to install argparse for Python 2.6.x .

--- repobuddy/tests/install-deps.sh
+++ repobuddy/tests/install-deps.sh
@@ -3,7 +3,7 @@ if python -c 'import sys as _sys; _sys.exit(_sys.version_info[0] != 2)'; then
     # Python 2
     if python -c 'import sys as _sys; _sys.exit(_sys.version_info[1] != 6)'; then
         # Python 2.6
-        pip install ordereddict unittest2
+        pip install argparse ordereddict unittest2
     fi
 fi
 pip install coverage
