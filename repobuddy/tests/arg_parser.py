5dc5b9619c9867236861aa4d947dc32095639113
Ash <tuxdude@StarScream>
2013-04-21 15:00:51 -0700

    Use _count_equal() instead of assertItemsEqual().
    Fix the tests which break on py3k.

--- repobuddy/tests/arg_parser.py
+++ repobuddy/tests/arg_parser.py
@@ -90,9 +90,9 @@ class ArgParserTestCase(TestCaseBase):
         groups = match_obj.groups()
 
         self.assertEqual(groups[0], 'repobuddy')
-        self.assertItemsEqual(groups[1].rstrip().split(' '), ['[-h]', '[-v]'])
-        self.assertItemsEqual(groups[4].rstrip().split(','),
-                              ['status', 'init', 'help'])
+        self._count_equal(groups[1].rstrip().split(' '), ['[-h]', '[-v]'])
+        self._count_equal(groups[4].rstrip().split(','),
+                          ['status', 'init', 'help'])
 
         return
 
